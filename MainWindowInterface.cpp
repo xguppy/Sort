96698e920619022b5528f56ae1fbcb01e6891761
Guppy <xguppy@yandex.ru>
2017-11-15 18:36:21 +0400

    Improves readability

--- MainWindowInterface.cpp
+++ MainWindowInterface.cpp
@@ -4,3 +4,35 @@
 
 #include "MainWindowInterface.h"
 
+MainWindowInterface::MainWindowInterface(int w, int h) :
+        Fl_Widget(0,0,w,h),
+        widget(950,50,200,200),
+        menuBar(0,0,(this->w()),50)
+{
+    menuBarInit();
+}
+
+void MainWindowInterface::menuBarInit() {
+    menuBar.add("Тестирование/Тема");
+    menuBar.add("Тестирование/Режим");
+    menuBar.add("Тестирование/Уровень");
+    menuBar.add("Тестирование/Таймер");
+    menuBar.add("О программе",0,showabout,(void*)this);
+    menuBar.add("Помощь");
+}
+
+void MainWindowInterface::showabout(Fl_Widget *w, void *) {
+    static About  win(200,400);
+    win.show();
+}
+
+MainWindowInterface::MyWidget::MyWidget(int x, int y, int w, int h, const char *L) :
+        Fl_Widget(x, y, w, h, L),
+        bt1(x, y + 120, (this->w()) - 40, 40, "Теория"),
+        bt2(x, y + 200, (this->w()) - 40, 40, "Демонстрация"),
+        bt3(x, y + 280, (this->w()) - 40, 40, "Тестирование") {
+    //TODO: СДЕЛАТЬ ПРИВЯЗКУ CALLBACK'ОВ
+    bt1.color(FL_LIGHT2);
+    bt2.color(FL_LIGHT2);
+    bt3.color(FL_LIGHT2);
+}
--- MainWindowInterface.h
+++ MainWindowInterface.h
@@ -18,48 +18,19 @@ class MainWindowInterface:public Fl_Widget
     private:
         Fl_Button bt1, bt2, bt3;
         void draw() override
-        {
-
-        }
+        {   }
     public:
-        MyWidget(int x, int y, int w, int h, const char *L = 0) :
-                Fl_Widget(x, y, w, h, L),
-                bt1(x, y + 120, (this->w()) - 40, 40, "Теория"),
-                bt2(x, y + 200, (this->w()) - 40, 40, "Демонстрация"),
-                bt3(x, y + 280, (this->w()) - 40, 40, "Тестирование") {
-            //TODO: СДЕЛАТЬ ПРИВЯЗКУ CALLBACK'ОВ
-            bt1.color(FL_LIGHT2);
-            bt2.color(FL_LIGHT2);
-            bt3.color(FL_LIGHT2);
-        }
+        MyWidget(int x, int y, int w, int h, const char *L = 0);
     };
     MyWidget widget;
     Fl_Menu_Bar menuBar;
-    static void showabout(Fl_Widget *w, void *) {
-        static About  win(200,400);
-        win.show();
-    }
-    void menuBarInit() {
-        menuBar.add("Тестирование/Тема");
-        menuBar.add("Тестирование/Режим");
-        menuBar.add("Тестирование/Уровень");
-        menuBar.add("Тестирование/Таймер");
-        menuBar.add("О программе",0,showabout,(void*)this);
-        menuBar.add("Помощь");
-    }
+    static void showabout(Fl_Widget *w, void *);
+    void menuBarInit();
 
     void draw() override
-    {
-
-    }
+    {    }
 public:
-    MainWindowInterface(int w, int h):
-            Fl_Widget(0,0,w,h),
-            widget(950,50,200,200),
-            menuBar(0,0,(this->w()),50)
-    {
-        menuBarInit();
-    }
+    MainWindowInterface(int w, int h);
 };
 
 #endif //SORT_MAINWINDOWINTERFACE_H
